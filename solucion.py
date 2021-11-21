48eb469036d4cc57e94758597397e5b3e7011186
sp4ceShrimp <vrosasz1700@alumno.ipn.mx>
2021-11-21 08:47:37 -0600

    1

--- solucion.py
+++ solucion.py
@@ -1,1 +1,33 @@
-## DECLARACIÓN DE VALORES ##
+########## SIMULACIÓN DE EVENTOS DISCRETOS ##########
+from pprint import pprint
+## DECLARACIÓN DE VALORES ###
+DIFS = 10e-3
+SIFS = 5e-3
+durRTS = 11e-3
+durCTS = 11e-3
+durACK = 11e-3
+durDATA = 43e-3
+sigma = 1e-3 #tamaño de miniranuras
+H = 7 #grados
+K = 15 #tamaño del buffer
+sleep = 18 #número de ranuras dormir
+#nodos_por_grado = [5,10,15,20]
+nodos_por_grado = [5]
+#windows_size = [16,32,64,128,256]
+windows_size = [16]
+#lambda1 = [0.0005,0.001,0.005,0.03]
+lambda1 = [0.0005]
+
+def inicializacion(N,W,lambdda):
+    T = sigma*W + DIFS + 3*SIFS + durRTS + durACK + durDATA
+    Tc = (2+lambdda)*T
+    nodos = [[0 for i in range(N)] for j in range(H)]
+    pprint(nodos)
+    print(Tc)
+
+for caso_nodos in nodos_por_grado:
+    for caso_W in windows_size:
+        for caso_lambda in lambda1:
+            print("Cantidad de nodos: {nodos} \nMáximo número de miniranuras: {W} \nTasa (lambda): {lambdaa}".format(nodos=caso_nodos,W=caso_W,lambdaa=caso_lambda))
+            #calculo duración de ranura, esta secuencia se deberá corregir en el diagrama
+            inicializacion(caso_nodos,caso_W,caso_lambda)
